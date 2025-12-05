900e527c36c4ea5b83d6a84b88717e2a2646c2e3
AVIRA <AVIRA@LAPTOP-U8B8IDQ6>
2025-12-05 15:13:11 +0530

    hi

--- semester1/src/semester1/array.java
+++ semester1/src/semester1/array.java
@@ -4,7 +4,7 @@ public class array {
 
 	public static void main(String[] args) {
 int[] array= {1,2,3,4,5};
-System.out.println("3rd element:"+array[2]);
+System.out.println("3rd elements:   "+array[2]);
 		// TODO Auto-generated method stub
 
 	}
