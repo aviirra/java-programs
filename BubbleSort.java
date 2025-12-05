e94296e77023c9f7fe96da4fac572d7d7d963fd1
aviirra <avirasashindran2004@gmail.com>
2025-12-05 13:52:41 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,21 @@
+package semester1;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		int []arr= {6,5,8,9,1,5};
+		int temp;
+		for(int i=0;i<arr.length-1;i++) {
+			for(int j=0;j<arr.length-1;j++) {
+				if(arr[i]>arr[j+1]) {
+					temp=arr[j];
+					arr[i]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		for(int ele:arr) {
+			System.out.println(ele+"");
+		}
+	}
+
+}
